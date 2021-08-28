f0ab753e37f538546734bba1d428b4ac6978f8b8
Harry <habs@sdf.org>
2021-08-27 22:31:26 -0400

    529 Absolute

--- questions/529-medium-absolute/template.ts
+++ questions/529-medium-absolute/template.ts
@@ -1,1 +1,5 @@
-type Absolute<T extends number | string | bigint> = any
+type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer N}` ? N : `${T}`
+
+{
+  const absolute: Absolute<-10> = "10"
+}
