e85b5718ea96a4d97aabf01e0069fe97d1902a22
Harry <habs@sdf.org>
2021-08-28 00:30:53 -0400

    949-medium-anyof

--- questions/949-medium-anyof/template.ts
+++ questions/949-medium-anyof/template.ts
@@ -1,1 +1,8 @@
-type AnyOf<T extends readonly any[]> = any;
+type AnyOf<T extends readonly any[]> = T extends [infer HEAD, ...infer REST]
+  ? HEAD extends 0 | '' | false | [] | { [P in any]: never } // falsey
+    ? AnyOf<REST> : true
+  : false
+
+  {
+    const anyOf: AnyOf<[0, '', false, 1]> = true
+  }
