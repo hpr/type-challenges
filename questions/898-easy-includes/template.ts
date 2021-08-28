db8b0dbbad80547cbc303d9508f81c6d298c96b4
Harry <habs@sdf.org>
2021-08-27 21:05:05 -0400

    898 includes

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,16 @@
-type Includes<T extends readonly any[], U> = any;
+type Includes<T extends readonly any[], U> = {
+  [X in T[number]]: 1
+}[U] extends 1 ? true : false
+
+// type Includes<T extends readonly any[], U> = U extends T[number] ? true : false
+
+// type Includes<T extends readonly unknown[], U> = T extends [infer HEAD, ...infer REST]
+//   ? U extends HEAD
+//     ? HEAD extends U
+//       ? true : false
+//     : Includes<REST, U>
+//   : false
+
+{
+  const includes: Includes<['a', 'b', 'c'], 'a'> = true
+}
