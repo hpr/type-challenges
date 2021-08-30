09a2a21ef0b2f677985a81847a523b181941d018
Harry <habs@sdf.org>
2021-08-29 23:18:19 -0400

    527 AppendToObject

--- questions/527-medium-append-to-object/template.ts
+++ questions/527-medium-append-to-object/template.ts
@@ -1,1 +1,13 @@
-type AppendToObject<T, U, V> = any
+// type AppendToObject<T, U, V> = {
+//   [K in keyof T]: T[K]
+// }
+
+// type AppendToObject<T, U extends string, V> = T & { [P in U]: V }
+
+type AppendToObject<T, U extends string, V> = {
+  [P in (keyof T) | U]: P extends keyof T ? T[P] : V
+}
+
+{
+  const appendToObject: AppendToObject<{}, 'name', ''> = { name: '' }
+}
