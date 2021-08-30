94aa332b43122e13b8af2123e16fbb4dde51c41a
Harry <habs@sdf.org>
2021-08-29 22:17:55 -0400

    191 AppendArgument

--- questions/191-medium-append-argument/template.ts
+++ questions/191-medium-append-argument/template.ts
@@ -1,1 +1,5 @@
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn, A> = Fn extends (...args: infer P) => infer R ? (...args: [...P, A]) => R : never
+
+{
+  const appendArgument: AppendArgument<Math['pow'], number> = (a, b, c) => a ** b + c
+}
